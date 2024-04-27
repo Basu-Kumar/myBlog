bfff8b5855687fe967f702e5a9e31587abd7928c
Basu Kumar <basutulsyan710@gmail.com>
2024-04-28 03:45:31 +0530

    error handler mechanism added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (
@@ -12,7 +13,7 @@ export const signup = async (req, res) => {
     email === "" ||
     password === ""
   ) {
-    res.status.json({ message: "All fields are required" });
+    next(errorHandler(400, "All fields are required"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -26,7 +27,7 @@ export const signup = async (req, res) => {
   try {
     await newUser.save();
     res.json("signup successfull");
-  } catch (err) {
-    res.status.json({ message: err.message });
+  } catch (error) {
+    next(error);
   }
 };
